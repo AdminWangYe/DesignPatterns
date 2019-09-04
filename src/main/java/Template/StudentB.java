e6b39e1a33fc2f8d4b3e14651834a7208207a5c9
AdminWangYe <latelove2014@gmail.com>
2019-09-04 11:16:24 +0800

    添加模板方法模式代码

---
+++ src/main/java/Template/StudentB.java
@@ -0,0 +1,24 @@
+package Template;
+
+/**
+ * @author: yyWang
+ * @create: 2019/9/4
+ * @description:
+ */
+
+public class StudentB extends TestPaper {
+    @Override
+    public String answerQuestion1() {
+        return "B";
+    }
+
+    @Override
+    public String answerQuestion2() {
+        return "C";
+    }
+
+    @Override
+    public String answerQuestion3() {
+        return "A";
+    }
+}
