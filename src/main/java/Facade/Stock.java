3a076522ad5a04f1714c3c897f9c2e465c031101
AdminWangYe <1473332533@qq.com>
2019-09-05 22:24:39 +0800

    外观模式例子，添加股票类，买卖操作

---
+++ src/main/java/Facade/Stock.java
@@ -0,0 +1,24 @@
+package Facade;
+
+/**
+ * @author: yyWang
+ * @create: 2019/9/5
+ * @description: 股票类，股票购买操作
+ */
+
+public class Stock {
+
+    private String name;
+
+    public Stock(String name) {
+        this.name = name;
+    }
+
+    public void buyStock() {
+        System.out.println("购买" + this.name + "股票");
+    }
+
+    public void sellStock() {
+        System.out.println("卖出" + this.name + "股票");
+    }
+}
